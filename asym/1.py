10ebfb885ef8b682d43056da961865cc7800977d
kkkyloo <92991764+kkkyloo@users.noreply.github.com>
2023-03-18 12:17:17 +0300

    Delete 1.py

--- asym/1.py
+++
@@ -1,9 +0,0 @@
-import rsa
-from rsa import key, common
-(pub_key, priv_key) = key.newkeys(256)
-message = b'hello'
-crypto = encrypt(message, pub_key)
-
-(pub_key, priv_key) = rsa.newkeys(256)
-crypto = encrypt(b'hello', pub_key)
-decrypt(crypto, priv_key)
